df85586985f5f6753ac3b00693b353a5e97d872d
Joseph Fischetti <fischettijw@gmail.com>
2023-04-06 13:41:19 -0400

    first

---
+++ DogClass/Dog.py
@@ -0,0 +1,38 @@
+class Dog():
+    def __init__(self, name, age, breed, characteristic):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.characteristic = characteristic
+        
+    def print_breeds(self):
+        for breed in self.breed:
+            print(breed)
+            
+    def print_characteristic(self):
+        for char in self.characteristic:
+            print(char)
+            
+    def print_dog(self):
+        print()
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Breed: {self.breed}")
+        print(f"Characteristic: {self.characteristic}")
+        print()
+            
+    def read_file(data_file_name):
+        file = open(data_file_name, "r")
+        file_data = file.read()
+        file.close()
+        return file_data
+    
+    def print_file(print_file_name, heading):
+        items = Dog.read_file(print_file_name)
+        print()
+        print(heading.upper())
+        print("=======================")
+        print(items)
+
+
+        
---
+++ DogClass/Dog_Breeds_Characteristics.py
@@ -0,0 +1,15 @@
+from Dog import *
+import os
+os.system("cls")
+
+joker = Dog("Joker", 13, "Boxer", "Family Pet")
+charlie = Dog("Charlie",9, "Pug", "Courageous")
+
+characteristics = Dog.read_file("DogCharacteristics.txt")
+breeds = Dog.read_file("DogBreeds.txt")
+
+Dog.print_file("DogCharacteristics.txt", "characteristics")
+Dog.print_file("DogBreeds.txt", "breeds")
+
+joker.print_dog()
+charlie.print_dog()
